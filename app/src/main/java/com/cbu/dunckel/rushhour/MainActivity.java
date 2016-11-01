6a5f0fa5a0678da2c26332421b6be3f021d32b32
Peter Dunckel <peterdunckel94@yahoo.com>
2016-10-31 17:55:35 -0700

    Initial Project Setup

---
+++ app/src/main/java/com/cbu/dunckel/rushhour/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cbu.dunckel.rushhour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
