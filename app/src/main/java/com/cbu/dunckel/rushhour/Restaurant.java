a643eaa963dda432fc1c802a5f8359097266ead5
Jacob De La Rosa-Torres <jacobmatthew.delarosatorres@calbaptist.edu>
2016-11-04 13:30:32 -0700

    Refactoring

---
+++ app/src/main/java/com/cbu/dunckel/rushhour/Restaurant.java
@@ -0,0 +1,24 @@
+package com.cbu.dunckel.rushhour;
+
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.media.Image;
+
+import java.net.URL;
+
+/**
+ * Created by jacobtorres on 11/4/16.
+ */
+
+public class Restaurant {
+
+    private String mName;
+    private Location mLocation;
+    private int mWaitTime;
+    private URL mMenuURL;
+    private int[] mAnalytics; // possible 672 data points (updating every 30 minutes)
+
+    
+
+
+}
