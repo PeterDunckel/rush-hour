5668a16e8eb0cec4cdd65ea204ddbc06a41f57dd
NickNatali <nicka.natali@calbaptist.edu>
2016-11-17 20:09:40 -0800

    When the menu button is clicked it opens up a dialog fragment displaying an actual menu. Also, there is now a geoActivity that gathers the user's location.

---
+++ app/src/main/java/com/cbu/dunckel/rushhour/GeoActivity.java
@@ -0,0 +1,96 @@
+package com.cbu.dunckel.rushhour;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+/**
+ * Created by NickNatali on 11/17/16.
+ */
+
+public class GeoActivity extends RestaurantListActivity implements LocationListener {
+
+    LocationManager locationManager;
+    String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location != null){
+            Log.i("Location Info", "Location acheived!"); } else {
+            Log.i("Location info", "No location :( ");
+        }
+
+    }
+//TODO: FIX THIS FUNCTION
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+   // getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+}
+
+
+@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.expandableRestaurantList) {
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+
+}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider,400,1,this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+        Log.i("Location info: Lat", lat.toString());
+        Log.i("Location info: lng", lng.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int Status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+}
--- app/src/main/java/com/cbu/dunckel/rushhour/dFragment.java
+++ app/src/main/java/com/cbu/dunckel/rushhour/dFragment.java
@@ -1,12 +1,31 @@
 package com.cbu.dunckel.rushhour;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by NickNatali on 11/12/16.
@@ -15,13 +34,85 @@ import android.widget.Toast;
 public class dFragment extends android.support.v4.app.DialogFragment {
 
     private static Context context;
+    private ImageView menuView;
+    private static boolean canGetURl = true;
+
 
     @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_restaurant_list, container, false);
-            getDialog().setTitle("Menu");
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.menu_fragment, container, false);
+        getDialog().setTitle("Menu");
 
-            return rootView;
-        }
+        menuView = (ImageView) rootView.findViewById(R.id.menuImage);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-}
+//        StrictMode.setThreadPolicy(policy);
+//        menuView.setImageDrawable(LoadImageFromWebOperations("app/res/drawable/ic_android_black_24dp.xml"));
+
+        Drawable myDrwable = getResources().getDrawable(R.drawable.menu);
+        menuView.setImageDrawable(myDrwable);
+//        UrlImageViewHelper.setUrlDrawable(menuView,"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd" +
+//                "=&cad=rja&uact=8&ved=0ahUKEwjCjbDWzqfQAhWDsJQKHaVNCzoQjRwIBw&url=https%3A%2F%2Fwww.canva.com%2Ftemp" +
+//                "lates%2Fmenus%2FMABKz_xy31k-red-minimalist-diner" +
+//                "-menu%2F&bvm=bv.138493631,d.dGo&psig=AFQjCNEDRqTAKIOB0ldjFiyC7CHBz2g9sA&ust=1479191160028591");
+        return rootView;
+    }
+
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+
+                try {
+                    InputStream is = (InputStream) new URL(url).getContent();
+                    Log.d("TAG", " Drawable");
+                    Drawable d = Drawable.createFromStream(is, "src name");
+                    Log.d("TAG", "Got Drawable" + d);
+                    return d;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+    }
+//
+//public class DownloadImagesTask extends AsyncTask<ImageView, Void, Bitmap> {
+//
+//    ImageView imageView = null;
+//
+//    @Override
+//    protected Bitmap doInBackground(ImageView... imageViews) {
+//        this.imageView = imageViews[0];
+//        return download_Image((String)imageView.getTag());
+//    }
+//
+//    @Override
+//    protected void onPostExecute(Bitmap result) {
+//        imageView.setImageBitmap(result);
+//    }
+//
+//    private Bitmap download_Image(String url) {
+//        Bitmap bm = null;
+//        try {
+//            URL aURL = new URL(url);
+//            URLConnection conn = aURL.openConnection();
+//            conn.connect();
+//            InputStream is = conn.getInputStream();
+//            BufferedInputStream bis = new BufferedInputStream(is);
+//            bm = BitmapFactory.decodeStream(bis);
+//            bis.close();
+//            is.close();
+//        } catch (IOException e) {
+//            Log.e("Hub","Error getting the image from server : " + e.getMessage().toString());
+//        }
+//        return bm;
+//    }
+//
+
+
+//}
+
+
+
+
+
+
